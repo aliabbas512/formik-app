905f9a25150f478c1fdf583ac568766000da1df2
Syed Ali Abbas <aliabbasshah72@gmail.com>
2023-12-13 15:51:17 +0500

    Form handling with formik+materialUI

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import "./App.css";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+//Create a Form with Formik
+function App() {
+  const initialValues = {
+    fullname: "",
+    email: "",
+    password: "",
+  };
+
+  //Form Submission With Formik
+  const onSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  };
+
+  //ValidationSchema with Yup
+  const validationSchema = Yup.object({
+    fullname: Yup.string().required("Full Name is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email address"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  return (
+    // add material-ui components
+    <Container maxWidth="sm">
+      <br/>
+      <center>
+        <Typography variant="h4" gutterBottom>
+          Register New Account
+        </Typography>
+        {/* Form Validation with Formik */}
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Grid container spacing={2} justifyContent="center" alignItems="center" direction="column">
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    type="text"
+                    name="fullname"
+                    label="Full Name"
+                    variant="outlined"
+                    placeholder="Enter your full name"
+                    fullWidth
+                  />
+                  <ErrorMessage name="fullname" component="div" />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    type="email"
+                    name="email"
+                    label="Email"
+                    variant="outlined"
+                    placeholder="Enter your email"
+                    fullWidth
+                  />
+                  <ErrorMessage name="email" component="div" />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    type="password"
+                    name="password"
+                    label="Password"
+                    variant="outlined"
+                    placeholder="Enter your password"
+                    fullWidth
+                  />
+                  <ErrorMessage name="password" component="div" />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button type="submit" disabled={isSubmitting} variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </center>
+    </Container>
+  );
+}
+
+export default App;
